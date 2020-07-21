e4fc7dd20b73880c052cf5f1d3662b6ad02313a1
Jephefnie <56341037+jephefnie@users.noreply.github.com>
2020-07-21 19:38:35 +0800

    Add files via upload

---
+++ fire/js/time.js
@@ -0,0 +1,31 @@
+(function() {
+    var time = {
+        curr: 60,
+        timer: null,
+        dom: document.getElementById('bgTime'),
+        start: function() {
+            this.timer = setInterval(function() {
+                if(this.curr <= 0) {
+                    clearInterval(this.timer);
+                    level.over(false);
+                    return ;
+                }
+                this.dom.innerHTML = --this.curr;
+            }.bind(this), 1000);
+        },
+        end: function() {
+            clearInterval(this.timer);
+            this.timer = setInterval(function() {
+                if(this.curr <= 0) {
+                    clearInterval(this.timer);
+                    alert('Game over，Your final score: ' + score.total);
+                    return ;
+                }
+
+                this.dom.innerHTML = --this.curr;
+                score.add(0.5);
+            }.bind(this), 16);
+        }
+    };
+    window.time = time; 
+})();
